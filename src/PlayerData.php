5051b5092eff87902969a502fcd2e140cd49eedc
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-09-02 20:15:18 +0200

    Improved fetching player data from ban list.

--- src/PlayerData.php
+++ src/PlayerData.php
@@ -8,13 +8,27 @@ class PlayerData
         global $config;
         $file = @fopen($config['bans']['tempfile'],"r");
         if($file){
-            while(($buffer = fgets($file, 4096)) !== false){
-                if(preg_match("/mdbBans banlist:/",$buffer)){
+            while(($buffer = fgets($file, 4096)) !== false && !feof($file)) {
+                if(strlen(trim($buffer)) == 0) {
                     continue;
                 }
-                $items = explode("--", $buffer);
-                array_push(self::$data,['PlayerName' => $items[0], 'PlayerId' => $items[1], 'PlayerSteamId' => $items[2]]);
+                $filtered = str_replace(['-%-'], '',$buffer);
+                $filtered= preg_replace("/\s\s+/",",",$filtered);
+                $filtered = preg_replace('/^([a-z+0-9\s]+)/','$1,',$filtered);
+                $filtered = preg_replace('/([a-z]+)([+0-9]+)/','$1',$filtered);
+                list($banType, $nick, $steamid, $ip, $mid, $date, $length,$bannedby,$reason) = explode(',',$filtered);
+                if($length == 0) {
+                    $length = "permanent";
+                }
+                if(!isset($reason)) {
+                    $reason = ".";
+                }
+                if(isset($banType) && isset($nick) && isset($steamid) && isset($ip) && isset($mid) && isset($date) && isset($length) && isset($bannedby) && isset($reason)) {
+                    array_push(self::$data,['PlayerName' => $nick, 'BanType' => $banType, 'PlayerId' => $mid,
+                        'PlayerSteamId' => $steamid, 'PlayerIP' => $ip, 'Date' => $date, 'BanLength' => $length, 'BannedBy' => $bannedby, 'reason' => $reason]);
+                }
             }
+            fclose($file);
             return true;
         }
         fclose($file);
